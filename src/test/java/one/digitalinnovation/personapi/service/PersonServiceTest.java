1d6cf5d9e7a6975080d44e541bf43664825f0533
Anderson <valladau2@gmail.com>
2021-06-26 12:09:25 -0300

    Developed unit tests for create person

--- src/test/java/one/digitalinnovation/personapi/service/PersonServiceTest.java
+++ src/test/java/one/digitalinnovation/personapi/service/PersonServiceTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import static one.digitalinnovation.personapi.utils.PersonUtils.createFakeDTO;
 import static one.digitalinnovation.personapi.utils.PersonUtils.createFakeEntity;
